4cc5fa11b80458628b5861cff8b2883d54ced72f
Ed Sweeney <ed@onextent.com>
2023-01-01 20:59:13 -0800

    more

--- src/json_to_state_actor.rs
+++ src/json_to_state_actor.rs
@@ -13,43 +13,51 @@ pub struct JsonStateActor {
     pub output: ActorHandle,
 }
 
+fn extract_values_from_json(text: &String) -> Result<HashMap<i32, f64>, String> {
+    let values: serde_json::Value = match serde_json::from_str(text.as_str()) {
+        Ok(values) => values,
+        Err(e) => return Err(e.to_string()),
+    };
+    let mut map: HashMap<i32, f64> = HashMap::new();
+    if let Some(obj) = values.as_object() {
+        for (key, value) in obj.iter() {
+            if let Ok(key) = key.parse::<i32>() {
+                if let Some(value) = value.as_f64() {
+                    map.insert(key, value);
+                } else {
+                    log::warn!("not numeric value: {}", value)
+                    // TODO: return error if respond_to is available
+                }
+            } else {
+                log::warn!("not numeric key: {}", key)
+                // TODO: return error if respond_to is available
+            }
+        }
+        Ok(map)
+    } else {
+        Err("invalid json".to_string())
+    }
+}
+
 #[async_trait]
 impl Actor for JsonStateActor {
     async fn handle_envelope(&mut self, envelope: MessageEnvelope) {
         match &envelope.message {
-            Message::PrintOneCmd { text } => {
-                let values: serde_json::Value = match serde_json::from_str(text.as_str()) {
-                    Ok(values) => values,
-                    Err(e) => {
-                        log::warn!("{}", e);
-                        return;
-                    }
-                };
-                let mut map: HashMap<i32, f64> = HashMap::new();
-                if let Some(obj) = values.as_object() {
-                    for (key, value) in obj.iter() {
-                        if let Ok(key) = key.parse::<i32>() {
-                            if let Some(value) = value.as_f64() {
-                                map.insert(key, value);
-                            } else {
-                                log::warn!("not numeric value: {}", value)
-                                // TODO: return error if respond_to is available
-                            }
-                        } else {
-                            log::warn!("not numeric key: {}", key)
-                            // TODO: return error if respond_to is available
-                        }
-                    }
+            Message::PrintOneCmd { text } => match extract_values_from_json(text) {
+                Ok(values) => {
+                    let msg = Message::UpdateCmd { values };
+                    self.output.tell(msg).await
                 }
-                let msg = Message::UpdateCmd { values: map };
-                self.output.tell(msg).await
-            }
+                Err(error) => {
+                    log::warn!("{}", error); // TODO send back an error to respond_to
+                }
+            },
             Message::IsCompleteMsg {} => {
                 let senv = MessageEnvelope {
                     message: envelope.message.clone(),
                     respond_to_opt: envelope.respond_to_opt,
                 };
-                self.output.send(senv).await
+                self.output.send(senv).await // forward the good news
             }
             _ => {}
         }
