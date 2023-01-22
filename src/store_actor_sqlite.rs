a19dc3abf08824d5e43430afc414adaa6f527df2
Ed Sweeney <ed@onextent.com>
2023-01-21 19:58:03 -0800

    db creation

--- src/store_actor_sqlite.rs
+++ src/store_actor_sqlite.rs
@@ -3,28 +3,17 @@ use crate::actor::ActorHandle;
 use crate::message::Message;
 use crate::message::MessageEnvelope;
 use async_trait::async_trait;
-use rusqlite::params;
-use rusqlite::{Connection, Result};
+use sqlx::SqlitePool;
+use std::env;
+use std::fs::File;
 use tokio::sync::mpsc;
 
-/// TODO: needs a file name and path that relates to the namespace
 pub struct StoreActor {
     pub receiver: mpsc::Receiver<MessageEnvelope>,
-    pub dbconn: Connection,
 }
 
 #[async_trait]
 impl Actor for StoreActor {
-    /// for LoadCmd:
-    ///   1. open db with the path as key
-    ///   2. write events to "stream_to"
-    ///   3. write next_message to "stream_to"
-    ///   4. write EndOfStream to "stream_to"
-    ///   5. closed send
-    /// for Update:
-    ///   1. open db
-    ///   2. insert Update msg with key as path
-    ///   3. closedb
     async fn handle_envelope(&mut self, envelope: MessageEnvelope) {
         let MessageEnvelope {
             message,
@@ -52,9 +41,15 @@ impl Actor for StoreActor {
                             .send(Message::EndOfStream {})
                             .await
                             .expect("can not integrate from helper");
-                        stream_to.closed().await;
                     }
                 }
+
+                // TODO: handle LoadCmd messages
+                // 1. open db with the path as key
+                // 2. write events to "stream_to"
+                // 3. write next_message to "stream_to"
+                // 4. write EndOfStream to "stream_to"
+                // 5. close send???
             }
             m => log::warn!("unexpected: {:?}", m),
         }
@@ -63,24 +58,41 @@ impl Actor for StoreActor {
 
 /// actor private constructor
 impl StoreActor {
-    fn new(receiver: mpsc::Receiver<MessageEnvelope>, dbconn: Connection) -> Self {
-        StoreActor { receiver, dbconn }
+    fn new(receiver: mpsc::Receiver<MessageEnvelope>) -> Self {
+        StoreActor { receiver }
     }
 }
 
 /// actor handle public constructor
 pub fn new(bufsz: usize) -> ActorHandle {
     async fn start(mut actor: StoreActor) {
+        let db_url = &env::var("DATABASE_URL").unwrap_or(String::from("navactor.db"));
+
+        match File::create(db_url) {
+            Ok(_) => log::debug!("File {} has been created", db_url),
+            Err(e) => log::warn!("Failed to create file {}: {}", db_url, e),
+        }
+
+        let dbconn = SqlitePool::connect(db_url).await.expect("");
+
+        // Create table if it doesn't exist
+        sqlx::query(
+            "CREATE TABLE IF NOT EXISTS updates (
+              path TEXT NOT NULL,
+              timestamp TEXT NOT NULL,
+              'values' TEXT NOT NULL
+        )",
+        )
+        .execute(&dbconn)
+        .await
+        .expect("cannot create table");
+
         while let Some(envelope) = actor.receiver.recv().await {
             actor.handle_envelope(envelope).await;
         }
     }
     let (sender, receiver) = mpsc::channel(bufsz);
-    let dbconn = Connection::open("my_db.sqlite3").unwrap(); // TODO: safe?
-
-    dbconn.execute("CREATE TABLE IF NOT EXISTS updates (path TEXT NOT NULL, timestamp TIMESTAMP NOT NULL, 'values' TEXT NOT NULL, PRIMARY KEY (path, timestamp))", params![])
-        .expect("cannot create db");
-    let actor = StoreActor::new(receiver, dbconn);
+    let actor = StoreActor::new(receiver);
     let actor_handle = ActorHandle::new(sender);
     tokio::spawn(start(actor));
     actor_handle
