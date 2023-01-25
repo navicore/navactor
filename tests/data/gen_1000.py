9fb4e1801afcda50de1f26b7cf8bef58b0b476f0
Ed Sweeney <ed@onextent.com>
2023-01-25 06:57:57 -0800

    cli

--- tests/data/gen_1000.py
+++ tests/data/gen_1000.py
@@ -8,18 +8,28 @@ import json
 import random
 import pytz
 from datetime import datetime, timedelta
+import argparse
 
 # Initialize starting datetime
 start_datetime = datetime(2023, 1, 3, 0, 0, 0, tzinfo=pytz.UTC)
 
+# Create an ArgumentParser object
+parser = argparse.ArgumentParser()
+
+# Add the "observations_per_minute" and "number_of_devices" arguments
+parser.add_argument("observations_per_minute", type=int, help="Number of observations per minute per device.")
+parser.add_argument("number_of_devices", type=int, help="Number of devices.")
+
+# Parse the command-line arguments
+args = parser.parse_args()
+
 # Calculate the number of observations to generate
-observations_per_minute = 10
 minutes_per_day = 24 * 60
 
-# Generate observations for 10 devices
-device_ids = list(range(1, 11))
+# Generate observations for the number of devices specified
+device_ids = list(range(1, args.number_of_devices + 1))
 
-for i in range(minutes_per_day * observations_per_minute):
+for i in range(minutes_per_day * args.observations_per_minute):
     # Shuffle the list of device IDs
     random.shuffle(device_ids)
     for device_id in device_ids:
@@ -27,7 +37,7 @@ for i in range(minutes_per_day * observations_per_minute):
         random_milliseconds = random.randint(-500, 500)
         # Increment datetime by 1 minute plus random milliseconds
         current_datetime = start_datetime + timedelta(
-                minutes=i / observations_per_minute,
+                minutes=i / args.observations_per_minute,
                 milliseconds=random_milliseconds)
         # Randomize values for keys 1, 2, and 3
         values = {"1": round(random.uniform(0, 100), 2),
