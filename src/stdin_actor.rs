faaed2014a764975f808de108eea5d6a60d51bdb
Ed Sweeney <ed@onextent.com>
2023-02-02 21:13:05 -0800

    none

--- src/stdin_actor.rs
+++ src/stdin_actor.rs
@@ -31,7 +31,7 @@ impl Actor for StdinActor {
 
             while let Some(text) = lines.next_line().await.unwrap_or_else(|e| {
                 log::error!("failed to read stream: {:?}", e);
-                return None;
+                None
             }) {
                 let msg = Message::PrintOneCmd { text };
                 match self.output.tell(msg).await {
