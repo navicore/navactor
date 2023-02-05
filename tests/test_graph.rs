1ae2b1d32c144ed29fbf5dd7192ed0afe6931315
Ed Sweeney <ed@onextent.com>
2023-02-04 21:49:13 -0800

    test-clippy

--- tests/test_graph.rs
+++ tests/test_graph.rs
@@ -9,7 +9,7 @@ fn test_graph() {
     assert_eq!(graph.node_count(), 2);
     assert_eq!(graph.edge_count(), 1);
 }
-
+#[cfg_attr(feature = "cargo-clippy", allow(clippy::unwrap_used))]
 #[test]
 fn test_graph_labels() {
     let mut graph = Graph::new();
