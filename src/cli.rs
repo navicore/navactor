87d8573e2cf815eab019a9980f1407d673d1f11d
Ed Sweeney <ed@onextent.com>
2023-02-09 19:57:40 -0800

    enable completion script gen

--- src/cli.rs
+++ src/cli.rs
@@ -1,13 +1,14 @@
 use clap::{Args, Parser, Subcommand};
 
-#[derive(Parser)]
+#[derive(Parser, Debug, PartialEq)]
 #[command(
+    name = "completion-derive",
     author,
     version,
     about,
-    long_about = "nv is the CLI for the DtLaboratory project"
+    long_about = "nv is the CLI for the DtLaboratory project",
+    propagate_version = true
 )] // Read from `Cargo.toml`
-#[command(propagate_version = true)]
 pub struct Cli {
     #[arg(
         short,
@@ -27,11 +28,13 @@ pub struct Cli {
     pub verbose: u8,
     #[arg(long, action = clap::ArgAction::SetTrue, help = "No on-disk db file", long_help = "For best performance, but you should not run with '--silent' as you won't know what the in-memory data was since it is now ephemeral.")]
     pub memory_only: Option<bool>,
-    #[command(subcommand)]
+    // #[command(subcommand)]
+    // pub command: Option<Commands>,
+    #[clap(subcommand)]
     pub command: Commands,
 }
 
-#[derive(Subcommand, Debug)]
+#[derive(Subcommand, Debug, PartialEq)]
 pub enum Commands {
     Update {
         #[arg(short, long, action = clap::ArgAction::SetTrue, help = "No output to console.", long_help = "Supress logging for slightly improved performance if you are loading a lot of piped data to a physical db file.")]
@@ -44,16 +47,20 @@ pub enum Commands {
         allow_duplicates: Option<bool>,
     },
     Inspect {
-        #[arg(short, long, action = clap::ArgAction::Set)]
+        #[arg(short, long, action = clap::ArgAction::Set, help = "get the state of an actor")]
         path: String,
     },
     Configure {
-        #[arg(short, long, action = clap::ArgAction::Set)]
+        #[arg(short, long, action = clap::ArgAction::Set, help = "set the gene for the path", long_help = "Set the gene for a path of actors - '/' being root and all actors, /somepath will set the gene for all actors under 'somepath', overriding the root gene.")]
         path: String,
         #[arg(short, long, action = clap::ArgAction::Set)]
         gene: String,
     },
+    Completion {
+        #[arg(short, long, action = clap::ArgAction::Set, help = "print script for shell tab completion", long_help = "Pipe the output of this command to a file or to a shell program as appropriate for 'bash', or 'zsh', etc...")]
+        shell: clap_complete::Shell,
+    },
 }
 
-#[derive(Args)]
+#[derive(Args, Debug, PartialEq)]
 struct NoArgs {}
