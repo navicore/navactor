f596178575000c266732c2453b48c49c162c02ae
Ed Sweeney <ed@onextent.com>
2023-02-12 18:16:45 -0800

    tests

--- tests/test_store_actor.rs
+++ tests/test_store_actor.rs
@@ -15,7 +15,9 @@ use tokio::sync::oneshot;
 async fn setup_actors(db_file_prefix: String, namespace: String) -> Handle {
     let output_actor = stdout_actor::new(8);
 
-    let store_actor = store_actor_sqlite::new(8, db_file_prefix, false, true);
+    // do not configured to tolerate collisions because the "allow dupes" setting uses envelope
+    // time and that causes collisions due to sub-millisecond execution of the test.
+    let store_actor = store_actor_sqlite::new(8, db_file_prefix, false, false);
 
     let director_w_persist = director::new(&namespace, 8, Some(output_actor), Some(store_actor));
 
@@ -45,10 +47,9 @@ async fn shutdown_actors(json_decoder_actor: Handle) {
     assert!(matches!(result_message, Ok(Message::EndOfStream {}),));
 }
 
-#[cfg_attr(feature = "cargo-clippy", allow(clippy::expect_used))]
 #[cfg_attr(feature = "cargo-clippy", allow(clippy::unwrap_used))]
 #[test]
-fn test_write_jrnl() {
+fn test_write_and_read_jrnl() {
     let namespace = String::from("/actors");
     let db_file_prefix = format!("/tmp/{namespace}");
 
@@ -78,7 +79,7 @@ fn test_write_jrnl() {
     };
 
     let ob_2_3_file = "tests/data/single_observation_2_3.json";
-    let _ob_2_3_json = match fs::read_to_string(ob_2_3_file) {
+    let ob_2_3_json = match fs::read_to_string(ob_2_3_file) {
         Ok(text) => text,
         Err(e) => {
             println!("Error reading file: {}", e);
@@ -107,7 +108,7 @@ fn test_write_jrnl() {
 
         // query state of actor one from above updates
         let cmd = Message::TextMsg {
-            text: get_actor_one_json,
+            text: get_actor_one_json.clone(),
             hint: MtHint::Query,
         };
 
@@ -131,12 +132,43 @@ fn test_write_jrnl() {
                 assert!(false, "{e}");
             }
         };
+
+        // insert 2nd update to the db
+        let cmd = Message::TextMsg {
+            text: ob_2_3_json,
+            hint: MtHint::Update,
+        };
+        let r = json_decoder_actor.tell(cmd).await;
+        assert_eq!(r.ok(), Some(()));
+
+        // query state of actor one from above updates
+        let cmd = Message::TextMsg {
+            text: get_actor_one_json,
+            hint: MtHint::Query,
+        };
+
+        match json_decoder_actor.ask(cmd).await {
+            Ok(r) => {
+                if let Message::StateReport {
+                    datetime: _,
+                    path,
+                    values,
+                } = r
+                {
+                    assert_eq!(path, "/actors/one");
+                    let keys: Vec<&i32> = values.keys().collect();
+                    assert_eq!(keys.len(), 1);
+                    assert_eq!(values.get(&3).unwrap(), &300.01);
+                } else {
+                    assert!(false, "bad response from output actor: {r:?}");
+                }
+            }
+            Err(e) => {
+                assert!(false, "{e}");
+            }
+        };
+
         // stop actors and close db
         shutdown_actors(json_decoder_actor).await;
     });
-}
-#[cfg_attr(feature = "cargo-clippy", allow(clippy::unwrap_used))]
-#[test]
-fn test_read_jrnl() {
-    //TODO
 }
